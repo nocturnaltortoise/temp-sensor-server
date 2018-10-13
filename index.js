d2bfb136213d5d3f2dfee977794b1e32561e5c73
Simon Turner <simonturner34@gmail.com>
2018-10-13 15:47:02 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const { Client } = require('pg');
+
+const configPath = './config.json';
+const parsedConf = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
+const client = new Client({
+  user: parsedConf.user,
+  database: parsedConf.database,
+  password: parsedConf.password
+});
+client.connect();
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.post('/reading', async (req, res) => {
+  const temperatureReading = req.body.reading;
+  const sensorMACAddress = req.body.mac;
+
+  const sensorQuery = "SELECT sensor_id FROM sensors WHERE mac_address = $1 LIMIT 1;"
+  const sensorQueryRes = await client.query(sensorQuery, [sensorMACAddress]);
+  const sensorId = sensorQueryRes.rows[0].sensor_id;
+  console.log(`Sensor ID is ${sensorId}`);
+
+  console.log(temperatureReading, sensorMACAddress);
+  const readingInsertQuery = "INSERT INTO temperature_readings (sensor_id, reading) VALUES ($1, $2)";
+  const readingInsertRes = await client.query(readingInsertQuery, [sensorId, temperatureReading]);
+  console.log(readingInsertRes.rows);
+  res.sendStatus(204);
+});
+
+app.listen(3000, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Server started on port 3000');
+});
